84ba92f80d0a0b83e12f282ab671fa1f68c0c993
rikicop <ruperto1@protonmail.com>
2021-01-02 18:38:45 -0400

    deploy my react app to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+const hello ='Hi my Friend'
+    return (
+
+      <div>
+    	  <h1>Helloo my dear {hello}!</h1>
+      </div>
+
+   );
+}
+
+export default App
---
+++ src/components/Greet.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+const Greet = () => <h1> Hello Ricardo  </h1>
